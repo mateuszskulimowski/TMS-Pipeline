82f93af18a63704f5ac40cbd9b80455e78ee4cc9
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-05-23 19:13:35 +0200

    add tab content smart in teams list

--- src/app/pages/teams/teams.page-module.ts
+++ src/app/pages/teams/teams.page-module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CenterLayoutComponentModule } from 'src/app/components/center-layout/center-layout.component-module';
 import { DescriptionComponentModule } from 'src/app/components/description/description.component-module';
+import { TabContentComponentModule } from 'src/app/components/tab-content/tab-content.component-module';
 import { TeamsListComponentModule } from 'src/app/components/teams-list/teams-list.component-module';
 import { TeamsPage } from './teams.page';
 
@@ -9,6 +10,7 @@ import { TeamsPage } from './teams.page';
     DescriptionComponentModule,
     TeamsListComponentModule,
     CenterLayoutComponentModule,
+    TabContentComponentModule,
   ],
   declarations: [TeamsPage],
   providers: [],
