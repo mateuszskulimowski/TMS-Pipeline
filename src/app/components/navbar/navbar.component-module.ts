4e3dc08949b95596f471fed471e77056e80345d9
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-05-23 19:17:01 +0200

    create navigation

--- src/app/components/navbar/navbar.component-module.ts
+++ src/app/components/navbar/navbar.component-module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule, RouterModule],
   declarations: [NavbarComponent],
   providers: [],
   exports: [NavbarComponent],
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,37 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { NavigationModel } from 'src/app/models/navigation.model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
+  private _hamburgerNavigationSubject: BehaviorSubject<boolean> =
+    new BehaviorSubject<boolean>(false);
+  public hamburgerNavigation$: Observable<boolean> =
+    this._hamburgerNavigationSubject.asObservable();
+
+  readonly navigationPanelName$: Observable<NavigationModel[]> = of([
+    { name: 'Home', route: '/' },
+    { name: 'Teams', route: '/teams' },
+    { name: 'Projects', route: '/projects' },
+    { name: 'Tasks', route: '/tasks' },
+    { name: 'Employees', route: '/employees' },
+  ]);
+
+  showNavigation(): void {
+    this._hamburgerNavigationSubject.value === false
+      ? this._hamburgerNavigationSubject.next(true)
+      : this._hamburgerNavigationSubject.next(false);
+  }
+  hideNavigation(): void {
+    this._hamburgerNavigationSubject.next(false);
+  }
 }
