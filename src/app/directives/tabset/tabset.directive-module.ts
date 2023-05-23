e9cbec09f2212091cba956d2b411aa7fe510664c
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-05-23 19:17:58 +0200

    create tabset directive

---
+++ src/app/directives/tabset/tabset.directive-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { TabsetDirective } from './tabset.directive';
+
+@NgModule({
+  imports: [],
+  declarations: [TabsetDirective],
+  providers: [],
+  exports: [TabsetDirective]
+})
+export class TabsetDirectiveModule {
+}
