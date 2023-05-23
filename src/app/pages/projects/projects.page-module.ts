afa0cba8b5502e49d9d19cfc0fdb54a837cd5c94
mateuszskulimowski <skulimowski.mateusz98@gmail.com>
2023-05-23 19:14:00 +0200

    add tab content smart in projects list

--- src/app/pages/projects/projects.page-module.ts
+++ src/app/pages/projects/projects.page-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CenterLayoutComponentModule } from 'src/app/components/center-layout/center-layout.component-module';
 import { DescriptionComponentModule } from 'src/app/components/description/description.component-module';
 import { ProjectsListComponentModule } from 'src/app/components/projects-list/projects-list.component-module';
+import { TabContentComponentModule } from 'src/app/components/tab-content/tab-content.component-module';
 import { ProjectsPage } from './projects.page';
 
 @NgModule({
@@ -9,6 +10,7 @@ import { ProjectsPage } from './projects.page';
     CenterLayoutComponentModule,
     DescriptionComponentModule,
     ProjectsListComponentModule,
+    TabContentComponentModule,
   ],
   declarations: [ProjectsPage],
   providers: [],
